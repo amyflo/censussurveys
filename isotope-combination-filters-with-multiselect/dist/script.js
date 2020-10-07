b02bcc525f4e9d8ea0ae95b04e2214648f43dca1
amyflo <amyflo@github.com>
2020-10-06 21:25:57 -0700

    Running searchbar

--- isotope-combination-filters-with-multiselect/dist/script.js
+++ isotope-combination-filters-with-multiselect/dist/script.js
@@ -1,100 +1,71 @@
-/*jshint browser:true, undef: true, unused: true, jquery: true */
+// quick search regex
+var qsRegex;
+var checkboxFilter;
 
+// init Isotope
+var $container = $('#container')
 
+var $output = $('#output');
 
-// store filter per group
-var filters = {};
+// filter with selects and checkboxes
+var $checkboxes = $('#filter input');
 
-var $container = $('#container');
-//var $filterDisplay = $('#filter-display');
+var $quicksearch = $('#quicksearch')
 
-$container.isotope();
-// do stuff when checkbox change
-$('.options').on( 'change', function( event ) {
+// debounce so filtering doesn't happen every millisecond
+function debounce(fn, threshold) {
+  var timeout;
+  threshold = threshold || 100;
+  return function debounced() {
+    clearTimeout(timeout);
+    var args = arguments;
+    var _this = this;
 
-  var $select = event.target;
-  var $selectVal = $( $select );
-  
-  var group = $selectVal.attr('value-group');
-
-  // create array for filter group, if not there yet
-  var filterGroup = filters[ group ];
-
-  if ( !filterGroup ) {
-    filterGroup = filters[ group ] = [];
-  }
-
-  //console.log(event.target.selectedOptions);
-
-  if ( event.target.value != ''  ) {
-    let collection = event.target.selectedOptions;
-    if(filterGroup.length){
-      clearGroup(filterGroup);
+    function delayed() {
+      fn.apply(_this, args);
     }
+    timeout = setTimeout(delayed, threshold);
+  };
+}
 
-    for (let i=0; i<collection.length; i++) {
-        if(!filterGroup.includes(collection[i].value)){
-          filterGroup.push( collection[i].value );
+function Filter() {
+  // map input values to an array
+  var inclusives = [];
+  // inclusive filters from checkboxes
+  $checkboxes.each(function(i, elem) {
+    // if checkbox, use value if checked
+    if (elem.checked) {
+      inclusives.push(elem.value);
+    }
+  });
+
+  // combine inclusive filters
+  var filterValue = inclusives.length ? inclusives.join(', ') : '*';
+
+  var text = $quicksearch.val()
+
+  $container.isotope({
+    filter: function() {
+      var $this = $(this)
+      var checkText = text == '' ? true : $this.text().includes(text)
+      var checkClass = inclusives.length == 0 ? true : false;
+      $.each(inclusives, function(index, c) {
+        var _class = $this.hasClass(c)
+        if (_class) {
+          checkClass = true;
+          return;
         }
-      }
-  } else {
-    // remove filter
-    clearGroup(filterGroup);
-  }
- 
-  var comboFilter = getComboFilter();
-  $container.isotope({ filter: comboFilter });
-  //$filterDisplay.text( comboFilter );
-  
+      })
+      return checkText && checkClass
+    }
+  })
+  $output.text(filterValue);
+}
+
+$quicksearch.on('input', debounce(function() {
+  Filter()
+}));
+
+$checkboxes.change(function() {
+  Filter()
 });
-
-function clearGroup(filterGroup){
-  var filterCount = filterGroup.length;
-    while(filterCount >= 0){
-      filterGroup.splice( filterCount, 1 );    
-      filterCount--;
-    }
-}
-
-function getComboFilter() {
-  var combo = [];
-  for ( var prop in filters ) {
-    var group = filters[ prop ];
-    if ( !group.length ) {
-      // no filters in group, carry on
-      continue;
-    }
-    // add first group
-    if ( !combo.length ) {
-      combo = group.slice(0);
-      continue;
-    }
-    // add additional groups
-    var nextCombo = [];
-    // split group into combo: [ A, B ] & [ 1, 2 ] => [ A1, A2, B1, B2 ]
-    for ( var i=0; i < combo.length; i++ ) {
-      for ( var j=0; j < group.length; j++ ) {
-        var item = combo[i] + group[j];
-        nextCombo.push( item );
-      }
-    }
-    combo = nextCombo;
-  }
-  var comboFilter = combo.join(', ');
-  return comboFilter;
-}
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
