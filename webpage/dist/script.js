0bafe38d23dbcda37e6020a7887f1d4f09c070a2
amyflo <amyflo@github.com>
2020-10-07 09:07:13 -0700

    Adding folders, styling, css,  cleaning search bar

--- webpage/dist/script.js
+++ webpage/dist/script.js
@@ -66,3 +66,19 @@ $quicksearch.on('input', debounce(function() {
 $checkboxes.change(function() {
   Filter()
 });
+
+const clearIcon = document.querySelector(".clear-icon");
+  const searchBar = document.querySelector(".quicksearch");
+
+  searchBar.addEventListener("keyup", () => {
+    if(searchBar.value && clearIcon.style.visibility != "visible"){
+      clearIcon.style.visibility = "visible";
+    } else if(!searchBar.value) {
+      clearIcon.style.visibility = "hidden";
+    }
+  });
+
+  clearIcon.addEventListener("click", () => {
+    searchBar.value = "";
+    clearIcon.style.visibility = "hidden";
+  })
