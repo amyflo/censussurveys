15500553c30c82e7e18d611eb8adf0ad508e76a0
amyflo <amyflo@github.com>
2020-10-28 16:44:59 -0700

    added javascript to auto add items to container

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
-var grid=$('.cards');
+var grid=$('.container');
 var filter=$('.btn-group input');
 var search=$('.quicksearch');
 var qsRegex;
 var buttonFilter;
+
+
+// templating
+
+createItems();
+
   
 grid.isotope({
   itemSelector: '.card',
@@ -46,87 +52,90 @@ filter.change(function(){
   grid.isotope();
 });
 
-
-
-
-SurveyNames = ["Annual Business Survey",
-  "American Community Survey",
- "Annual Survey of Manufactures",
- "Business Dynamics Statistics", 
- "Building Permits",
- "County Business Patterns",
- "Commodity Flow Survey",
- "Decennial Census", 
- "e-Commerce Statistics",
- "Economic Census",
- "U.S. International Trade in Goods and Services",
- "Job-to-Job Flows",
- "LEHD Origin-Destination Employment Statistics",
- "Manufacturers' Shipments, Inventories, and Orders",
- "Monthly Retail Trade Survey",
- "Monthly Wholesale Trade Survey",
- "Nonemployer Statistics",
- "New Residential Construction",
- "New Residential Sales",
- "Quarterly Financial Report",
- "Quarterly Workforce Indicators"
-];
-
-SurveyGeography = [
-  "National, State, MetroArea, County, Place",
-  "National, State, MetroArea, County, Place, ZCTA, Tract, BlockGroup, OtherGeo",
-  "National, State",
-  "National, State, MetroArea",
-  "National, State, MetroArea, County, Place, Other",
-  "National, State, MetroArea, County, ZipCode, Other",
-  "National, State, Other",
-  "National, State, County, Place, ZCTA, Tract, BlockGroup, Block, OtherGeo",
-  "National",
-  "National, State, MetroArea, County, EconomicPlace, Place",
-  "National, State, MetroArea, OtherGeo",
-  "National, State, MetroArea",
-  "National, State, MetroArea, County, Place, ZCTA, Tract, BlockGroup, Block, OtherGeo",
-  "National",
-  "National",
-  "National",
-  "National, State, MetroArea, County",
-  "National, Other",
-  "National, Other",
-  "National",
-  "National, State, MetroArea, County, OtherGeo"
-];
-
-
-
-var colors = [ 'red', 'green', 'blue', 'orange' ];
-var sizes = [ 'small', 'medium', 'large' ];
-var prices = [ 10, 20, 30 ];
-
-createItems();
-
 function createItems() {
-  var brand, productType, color, size;
-  var items = '';
-  // dynamically create content
-  for (var i=0, len1 = data.brands.length; i < len1; i++) {
-    brand = data.brands[i];
-    for (var j=0, len2 = data.productTypes.length; j < len2; j++) {
-      productType = data.productTypes[j];
-        for (var l=0, len3 = data.colors.length; l < len3; l++) {
-        color = data.colors[l];
-        for (var k=0, len4 = data.sizes.length; k < len4; k++) {
-          size = data.sizes[k];
-          var itemHtml = '<div class="item ' + brand + ' ' +
-            productType + ' ' + color + ' ' + size + '">' +
-            '<p>' + brand + '</p>' +
-            '<p>' + productType + '</p>' +
-            '<p>' + size + '</p>' +
-            '</div>';
-            items += itemHtml;
-        }
-      }
-    }
-  }
 
-  $container.append( items );
+  var names = [
+    'Annual Business Survey',
+    'American Community Survey',
+    'Annual Survey of Manufactures',
+    'Business Dynamics Statistics',
+    'Building Permits',
+    'County Business Patterns',
+    'Commodity Flow Survey',
+    'Decennial Census',
+    'e-Commerce Statistics',
+    'Economic Census',
+    'U.S. International Trade in Goods and Services',
+    'Job-to-Job Flows',
+    'LEHD Origin-Destination Employment Statistics',
+    'Manufacturers Shipments, Inventories, and Orders',
+    'Monthly Retail Trade Survey',
+    'Monthly Wholesale Trade Survey',
+    'Nonemployer Statistics',
+    'New Residential Construction',
+    'New Residential Sales',
+    'Quarterly Financial Report',
+    'Quarterly Workforce Indicators'
+  ]
+  
+  var frequencies = [
+    'Annual', 
+    'Annual',
+    'Annual',
+    'Monthly',
+    'Annual',
+    'Other',
+    'Decennial',
+    'Annual',
+    'Other',
+    'Monthly',
+    'Quarterly',
+    'Annual',
+    'Monthly',
+    'Monthly',
+    'Monthly',
+    'Annual',
+    'Monthly',
+    'Monthly',
+    'Quarterly',
+    'Quarterly'
+  ];
+
+  var geographies = [
+    'National State Metro County Place',
+    'National State Metro County Place ZCTA Tract BlockGroup OtherGeo',
+    'National State',
+    'National State Metro',
+    'National State Metro County Place OtherGeo',
+    'National State Metro County Zip OtherGeo',
+    'National State OtherGeo',
+    'National State County Place ZCTA Tract BlockGroup Block OtherGeo',
+    'National',
+    'National State Metro County Economic Place',
+    'National State Metro OtherGeo',
+    'National State Metro',
+    'National State Metro County Place ZCTA Tract BlockGroup Block OtherGeo',
+    'National',
+    'National',
+    'National',
+    'National State Metro County',
+    'National OtherGeo',
+    'National OtherGeo',
+    'National',
+    'National State Metro County OtherGeo'
+  ];
+
+  var $items;
+
+  for (var i=0; i<names.length; i++){
+    var name = names[i];
+    var freq = frequencies[i];
+    var geo = geographies[i].replace(',', '');
+    var $item = $('<div />', {
+      'class': 'card ' + freq + ' ' + geo
+    });
+    $item.append('<p>' + name + '</p>');
+    $items = $items ? $items.add( $item ) : $item;
+  }
+  $items.appendTo( $('#container') );
 }
