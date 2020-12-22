e17eff1519177ac4b3eb7aec7f9827d619210a33
amyflo <amyflo@github.com>
2020-12-22 11:45:27 -0800

    Creating classfunctions to clear items depending  on selection

--- test.js
+++ test.js
@@ -1,120 +1,53 @@
-var grid=$("#grid");
-var filter=$(".btn-group input");
-var search=$(".quicksearch");
-var qsRegex;
-var buttonFilter;
 var Airtable = require('airtable');
 // Get a base ID for an instance of art gallery example
 var base = new Airtable({ apiKey: 'keyYJ7zM4UWu6RYdT' }).base('appAzdZwdzJzHt494');
-var surveys = {};
-// create empty array []
-// append items after for each loop
-// make that a separate function
-// everytime you change loop through again
-// airtable is just filtering the array
 
-function createSurveys() {
-    $('#grid').empty();
-    base('Surveys').select({
-        view: "Developer",
-        fields: ["Survey", "Description", "Link", "Frequency", "Geography", "Type", "TopicSelect"],
-    }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-            // console.log('Retrieved ', record);
+let records = []
 
-            // retrieving survey info
-            surveyname = record.get('Survey');
-            freqs = record.get('Frequency').join(', ');
-            geos = record.get('Geography').join(', ');
-            link = record.get('Link')
-            text = record.get('Description');
+// called for every page of records
+const processPage = (partialRecords, fetchNextPage) => {
+  records = [...records, ...partialRecords]
+  fetchNextPage()
+}
 
-            // creating filter names to match buttons
-            geosfilter = geos.replace("Block Group", "BlockGroup");
-            geosfilter = geosfilter.replace("Economic Place", "Economic");
-            geosfilter = geosfilter.replace("Metro Area", "Metro");
-            geosfilter = geosfilter.replace("Census Tract", "Census");
-            geosfilter = geosfilter.replace("Zip Code", "Zip");
-            geosfilter = geosfilter.replace("Other", "OtherGeo");
-            geosfilter = geosfilter.replace(/,/g, '');
-
-            // creating mapped object
-            surveys[surveyname] = {}
-            surveyinfo = surveys[surveyname]
-            surveyinfo["freqs"] = freqs;
-            surveyinfo["geos"] = geos;
-            surveyinfo["freqs"] = link;
-            console.log(surveys);
-            
-            
-            var linktext = "Explore Now";
-            var $card= $('<div/>', {
-                "class":'card card-body ' + freqs + ' ' + geosfilter ,
-            });
-            
-            // $card.append($("<h5 class='card-title'>").text(surveyname));
-
-            // $card.append($("<h6 class='card-subtitle mb-2 text-muted'>").text('Frequency: ' + freqs)); 
-            // $card.append($("<h6 class='card-subtitle mb-2 text-muted'>").text('Geography: ' + geos)); 
-            // $card.append($("<p class='card-text'>").text(text)); 
-
-            // $card.append("<div class='btn btn-primary'" + linktext.link(link) + "</div>");
-
-            // $('#grid').append($card);
-        });
-
-        fetchNextPage();
-    }, function done(error) {
-        console.log(error);
-    });
-};
-
-
-// var state = empty array
-// manipulate array
-
-createSurveys();
-
-
-// filter items 
-grid.isotope({
-    itemSelector: ".card",
-    filter: function() {
-        // console.log(searchResult);
-        var searchResult = qsRegex ? $(this).text().match( qsRegex ) : true;
-        var buttonResult = buttonFilter ? $(this).is( buttonFilter ) : true;
-        return searchResult && buttonResult;
-    }
-});
-
-// use value of search field to filter
-var quicksearch = search.keyup( debounce( function() {
-    qsRegex = new RegExp( quicksearch.val(), "gi" );
-    grid.isotope();
-}, 200 ) );
-  
-  // debounce so filtering doesn"t happen every millisecond
-  function debounce( fn, threshold ) {
-    var timeout;
-    return function debounced() {
-    if ( timeout ) {
-      clearTimeout( timeout );
-    }
-    function delayed() {
-      fn();
-      timeout = null;
-    }
-    timeout = setTimeout( delayed, threshold || 100 );
-    };
+// called when all the records have been retrieved
+const processRecords = (err) => {
+  if (err) {
+    console.error(err)
+    return
   }
-  
-    // change filters
-    filter.change(function(){
-        var filters = [];
-        filter.filter(":checked").each(function(){
-            filters.push( this.value );
-        });
-        filters = filters.join("");
-        buttonFilter = filters;
-        grid.isotope();
+records.forEach(function(record) {
+    console.log('Retrieved ', record.get('Name'));
+    freqs = record.get('Frequency').join(', ');
+    geos = record.get('Geography').join(', ');
+    link = record.get('Link')
+    text = record.get('Description');
+    geofilters = createGeofilters(geos);
+
+    // create cards
+    var $cardInfo = $('<div/>', {
+        "class":'card card-body show ' + freqs + ' ' +  geofilters
     });
+    $cardInfo.append($('<h5>').text(record.get('Survey')));
+    $cardInfo.append($('<div>').text(record.get('Description')));
+    $cardInfo.attr('data-record-id', record.getId());
+    $('#cards').append($cardInfo);
+});
+}
+
+// selecting the base and appropriate fields
+base('Surveys').select({
+    view: "Developer",
+    fields: ["Survey", "Description", "Link", "Frequency", "Geography", "Type", "TopicSelect"],
+}).eachPage(processPage, processRecords)
+
+// create class name text  for the geography to match selectors
+function createGeofilters(geos){
+    geofilters = geos.replace("Block Group", "BlockGroup");
+    geofilters = geofilters.replace("Economic Place", "Economic");
+    geofilters = geofilters.replace("Metro Area", "Metro");
+    geofilters = geofilters.replace("Other", "OtherGeo");
+    geofilters = geofilters.replace(/,/g, '');
+    return geofilters;
+}
+
